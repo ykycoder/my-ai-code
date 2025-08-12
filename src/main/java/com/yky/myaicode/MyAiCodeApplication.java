8847df8f33f2baaa70e5d10ea42d2cbdf43ab81b
ykycoder <649074096@qq.com>
2025-08-12 22:15:35 +0800

    初始化项目结构

---
+++ src/main/java/com/yky/myaicode/MyAiCodeApplication.java
@@ -0,0 +1,13 @@
+package com.yky.myaicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAiCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yky/myaicode/MyAiCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yky.myaicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAiCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
