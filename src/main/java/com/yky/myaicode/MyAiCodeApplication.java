47f3cab660d3dd9c7d651cae0ed56ed7dbfe2877
ykycoder <649074096@qq.com>
2025-08-17 20:56:28 +0800

    用户模块后端

--- src/main/java/com/yky/myaicode/MyAiCodeApplication.java
+++ src/main/java/com/yky/myaicode/MyAiCodeApplication.java
@@ -1,9 +1,11 @@
 package com.yky.myaicode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yky.myaicode.mapper")
 public class MyAiCodeApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/yky/myaicode/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.yky.myaicode.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.yky.myaicode.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/ykycoder">ykycoder</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
